b6464c0e3dd16ec8e01c613cc098e539ddd6632b
jituumakanta <jitu.umakanta@gmail.com>
2017-11-05 21:27:55 +0530

    5 added

--- app/src/main/java/com/news/githubtestbyme/MainActivity.java
+++ app/src/main/java/com/news/githubtestbyme/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
     }
 }
